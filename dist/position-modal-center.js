021de6aa4ad8440067f570848b8b849ff541c15e
Kyle Peatt <kpeatt@gmail.com>
2014-08-24 20:05:17 -0700

    Add animation to modal-center position

--- dist/position-modal-center.js
+++ dist/position-modal-center.js
@@ -14,13 +14,64 @@
         factory(window.Zepto || window.jQuery);
     }
 }(function($) {
-    return function($element) {
-        var $window = $(window);
+    return {
+        open: function() {
+            var plugin = this;
+            var $window = $(window);
 
-        $element
-            .css({
-                top: Math.max(0, (($window.height() - $element.height()) / 2) + $window.scrollTop()) + 'px',
-                left: Math.max(0, (($window.width() - $element.width()) / 2) + $window.scrollLeft()) + 'px'
-            });
+            if (this._isPercent(this.options.coverage)) {
+                var coverage = (this._coverageCalc(this.options.coverage) / 2) + '%';
+            }
+
+            this.$pinny
+                .css({
+                    top: coverage ? coverage : (($window.height() - this.$pinny.height()) / 2),
+                    bottom: coverage ? coverage : ($window.height() - this.$pinny.height()) / 2,
+                    right: coverage ? coverage : ($window.width() - this.$pinny.width()) / 2,
+                    left: coverage ? coverage : ($window.width() - this.$pinny.width()) / 2,
+                    width: coverage ? 'auto' : this.options.coverage,
+                    height: coverage ? 'auto': this.options.coverage
+                })
+                .velocity({ scale: [0, 0] }, 0)
+                .velocity(
+                    {
+                        scale: 1
+                    },
+                    {
+                        begin: function() {
+                            $('html')
+                                .css('overflow', 'hidden');
+                        },
+                        easing: this.options.easing,
+                        duration: this.options.duration,
+                        display: 'flex',
+                        complete: function() {
+                            $(document).off('touchmove', plugin.blockScroll);
+                        }
+                    }
+                );
+        },
+        close: function() {
+            var plugin = this;
+
+            this.$pinny
+                .velocity(
+                    'reverse',
+                    {
+                        begin: function() {
+                            $(document).on('touchmove', plugin.blockScroll);
+
+                            $('html')
+                                .css('overflow', '');
+                        },
+                        easing: this.options.easing,
+                        duration: this.options.duration,
+                        display: 'none',
+                        complete: function() {
+                            $(document).off('touchmove', plugin.blockScroll);
+                        }
+                    }
+                );
+        }
     };
 }));
