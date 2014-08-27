7ed3af58d7f92af02abce83a572f82eeb7c03faa
Steve Calvert <scalvert@mobify.com>
2014-08-27 10:31:56 -0700

    Refining examples

--- examples/assets/js/main.js
+++ examples/assets/js/main.js
@@ -1,18 +1,57 @@
 require(['config'], function() {
     require([
         'modal-center',
+        'sheet-top',
+        'sheet-right',
+        'sheet-left',
+        'sheet-bottom',
         '$',
         'pinny'
     ],
-    function(position, $) {
-        // Initialize Pinny
-        var $pinny = $('#somePinny').pinny({
-            position: position,
+    function(modalCenter, sheetTop, sheetRight, sheetLeft, sheetBottom, $) {
+        var $modalCenter = $('#modalCenterPinny').pinny({
+            position: modalCenter,
             coverage: '80%'
         });
 
-        $('.pinnyActivator').on('click', function() {
-            $pinny.pinny('toggle');
+        $('#modalCenter').on('click', function() {
+            $modalCenter.pinny('toggle');
+        });
+
+        var $sheetTop = $('#sheetTopPinny').pinny({
+            position: sheetTop,
+            coverage: '80%'
+        });
+
+        $('#sheetTop').on('click', function() {
+            $sheetTop.pinny('toggle');
+        });
+
+        var $sheetRight = $('#sheetRightPinny').pinny({
+            position: sheetRight,
+            coverage: '80%'
+        });
+
+        $('#sheetRight').on('click', function() {
+            $sheetRight.pinny('toggle');
+        });
+
+        var $sheetLeft = $('#sheetLeftPinny').pinny({
+            position: sheetLeft,
+            coverage: '80%'
+        });
+
+        $('#sheetLeft').on('click', function() {
+            $sheetLeft.pinny('toggle');
+        });
+
+        var $sheetBottom = $('#sheetBottomPinny').pinny({
+            position: sheetBottom,
+            coverage: '80%'
+        });
+
+        $('#sheetBottom').on('click', function() {
+            $sheetBottom.pinny('toggle');
         });
 
         // Enable active states
