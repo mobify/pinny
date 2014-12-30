48e41919379503514a2e217588bf8acfec302488
Stephen Cheung <Stephen@Stephens-MacBook-Air.local>
2014-12-30 14:36:38 -0800

    removed some comments

--- src/js/jquery_wrapper.js
+++ src/js/jquery_wrapper.js
@@ -2,7 +2,7 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
     define("$", (function (global) {
 	    return function () {
 	        var ret, fn;
-	        return ret || global.$; // what does this mean
+	        return ret || global.$;
 	    };
 	}(this)));
 }
--- src/js/velocity_wrapper.js
+++ src/js/velocity_wrapper.js
@@ -1,6 +1,5 @@
 (function(factory) {
     /* AMD module. */
-    // i think this means velocity depends on jquery/zepto to be loaded first?
     if (typeof define === "function" && define.amd) {
         define('velocity',['$'], factory);
     } else {
