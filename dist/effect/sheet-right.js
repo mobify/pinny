2c2d814e771e3b19440a80191e5850a8d138eacc
Ben Chypak <bchypak@mobify.com>
2015-06-23 15:03:54 -0700

    Add logic for left pinny

--- dist/effect/sheet-right.js
+++ dist/effect/sheet-right.js
@@ -24,34 +24,64 @@
                 height: 'auto'
             });
 
+        var currentCoverage = 0;
+        var setVisibility = function (dPercent) {
+            console.log('State: ', dPercent);
+
+            if (dPercent < 0 || dPercent > 100) {
+                return;
+            }
+
+            dPercent = 100 + dPercent;
+            // Translate to percentage open on screen
+            plugin.$pinny.css('-webkit-transform', 'translateX(' + dPercent + '%)');
+            plugin.$pinny.css('transform', 'translateX(' + dPercent + '%)');
+
+            currentCoverage = dPercent;
+
+            if (dPercent > -100) {
+                plugin.$pinny.css('display', 'block');
+            } else {
+                plugin.$pinny.css('display', 'none');
+            }
+        };
+
         return {
-            //openDirection: Hammer.DIRECTION_LEFT,
-            open: function() {
+            openDirection: Hammer.DIRECTION_LEFT,
+            open: function(dPercent) {
 
-
-                // Force feed the initial value
-                Velocity.animate(
-                    plugin.$pinny,
-                    { translateX: [0, '100%'] },
-                    {
-                        easing: plugin.options.easing,
-                        duration: plugin.options.duration,
-                        display: 'block',
-                        complete: plugin.animation.openComplete.bind(plugin)
-                    }
-                );
+                if (dPercent) {
+                    setVisibility(dPercent);
+                } else {
+                    // Force feed the initial value
+                    Velocity.animate(
+                        plugin.$pinny,
+                        { translateX: currentCoverage ? [0, currentCoverage + '%'] : [0, '100%'] },
+                        {
+                            easing: plugin.options.easing,
+                            duration: plugin.options.duration,
+                            display: 'block',
+                            complete: plugin.animation.openComplete.bind(plugin)
+                        }
+                    );
+                }
             },
-            close: function() {
-                Velocity.animate(
-                    plugin.$pinny,
-                    'reverse',
-                    {
-                        easing: plugin.options.easing,
-                        duration: plugin.options.duration,
-                        display: 'none',
-                        complete: plugin.animation.closeComplete.bind(plugin)
-                    }
-                );
+            close: function(dPercent) {
+
+                if (dPercent) {
+                    setVisibility(100 - dPercent);
+                } else {
+                    Velocity.animate(
+                        plugin.$pinny,
+                        { translateX: currentCoverage ? ['100%',  currentCoverage + '%'] : ['100%', 0] },
+                        {
+                            easing: plugin.options.easing,
+                            duration: plugin.options.duration,
+                            display: 'none',
+                            complete: plugin.animation.closeComplete.bind(plugin)
+                        }
+                    );
+                }
             }
         };
     };
