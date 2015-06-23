1433abb5548637b0fc43af33cd7308d15c738c40
Ben Chypak <bchypak@mobify.com>
2015-06-23 12:29:07 -0700

    Fix closing

--- dist/pinny.js
+++ dist/pinny.js
@@ -184,7 +184,7 @@
         },
 
         open: function(percentage) {
-            if (this._isOpen()) {
+            if (this._isOpen() || !this._isOpening()) {
                 return;
             }
 
@@ -209,7 +209,7 @@
         },
 
         close: function(percentage) {
-            if (!this._isOpen()) {
+            if (!this._isOpen() || this._isOpening()) {
                 return;
             }
 
