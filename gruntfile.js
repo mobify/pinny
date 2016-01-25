0139ea7578068d5919fcdaf508cc0655e8d23627
fractaltheory <michael@mobify.com>
2016-01-25 14:28:47 -0800

    Remove grunt release task

--- gruntfile.js
+++ gruntfile.js
@@ -20,12 +20,6 @@ module.exports = function(grunt) {
         config[configName] = _.extend(config[configName] || {}, option);
     });
 
-    grunt.initConfig(_.extend({
-        pkg: grunt.file.readJSON('package.json'),
-        releaseName: '<%= pkg.name %>-<%= pkg.version %>',
-        releaseMessage: '<%= pkg.name %> release <%= pkg.version %>'
-    }, config));
-
     // load npm tasks
     var npmTasks = [
         'grunt-contrib-uglify',
@@ -34,7 +28,6 @@ module.exports = function(grunt) {
         'grunt-css',
         'grunt-concurrent',
         'grunt-open',
-        'grunt-shell',
         'grunt-contrib-clean',
         'grunt-contrib-copy',
         'grunt-autoprefixer',
@@ -52,7 +45,6 @@ module.exports = function(grunt) {
     grunt.registerTask('serve', ['build', 'connect:server', 'watch']);
     grunt.registerTask('examples', ['build', 'connect:server', 'open:examples', 'watch']);
     grunt.registerTask('build', ['lint:dev', 'copy', 'uglify', 'version:all', 'sass', 'autoprefixer', 'cssmin']);
-    grunt.registerTask('release', ['lint:dev', 'test', 'shell:tagRelease']);
     grunt.registerTask('test', ['build', 'connect:test', 'mocha_phantomjs']);
     grunt.registerTask('test:browser', ['build', 'concurrent:tests']);
     grunt.registerTask('default', 'build');
